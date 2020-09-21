61fb8827f58a1d954076041b9711eb4215d93070
angiedanielar <angie.ruiz@mail.escuelaing.edu.co>
2020-09-20 19:42:35 -0500

    corrigiendo parte 3

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,53 +2,54 @@ var app = (function () {
 
     var _cinema;
     var _date;
-    var _functions;
 
     function _setCinemaName(cinema) {
-        _cinema = cinema;
-    }
-    ;
+        _cinema = $("cinema");
+    };
 
     function _setDate(date) {
-        _date = date;
-    }
-    ;
+        _date = $("date");
+    };
 
-    function setCinemaName(cinema) {
-        _setCinemaName(cinema);
-    }
-    ;
 
-    function setDate(date) {
-        _setDate(date);
-    }
-    ;
-
-    function _updateFunctionsData(functions) {
-        _functions = [];
-        functions.map(function (f) {
-            _functions.push({movieName: f.movie.name, genre: f.movie.genre, hour: f.date.substring(11, 16)});
-        });
-        _updateTableData();
-    }
-
-    function _updateTableData() {
-        $("#functionsTable > tbody").empty()
-        _functions.map(function (movie) {
-            var onClick = "app.openSeats(\"" + movie.hour + "\")";
-            var btn = "<button class='btn btn-outline-primary' value='Open Seats' onclick=" + onClick + ">Open seats</input>";
-            var fila = '<tr><td>' + movie.movieName + '</td><td>' + movie.genre + '</td><td>' + movie.hour + '</td><td>' + btn + '</tr>';
-            $("#functionsTable > tbody").append(fila)
+    function _map(functions) {
+        return mapList = functions.map(function(cinemaFunction){
+            return {
+                nombre:cinemaFunction.movie.name,
+                genero:cinemaFunction.movie.genre,
+                hora:cinemaFunction.date.split(" ")[1]
+            }
         })
-        _showMovies();
+
     }
 
-    function _showMovies() {
-        $("#cinemaSelected").text("Cinema Selected: " + _cinema);
-        $("#movies").text("Movies:");
-        document.getElementById('functions').style.visibility = "visible";
+    function _mapOneByOne(cinemaFunctions){
+        functions = _map(cinemaFunctions);
+        var tabla = $("cines");
+        var body = $("tbody");
+        if (body != null) {
+            body.remove();
+        }
+        tabla.append("<tbody>");
+        var tblBody = $("tbody");
+        functions.map(function(f){
+            var fila = '<tr><td>' + f.nombre + '</td><td>' + f.genero + '</td><td>' + f.hora + '</td><td>';
+            tblBody.append(fila);
+        })
+        tabla.append(tblBody);
+        tabla.append("</tbody>");
+
     }
 
-}
-)
+    function getFunctions(){
+        cinemaName = $("#name").val();
+        cinemaDate = $("#date").val();
+        apimock.getFunctionsByCinemaAndDate(cinemaName,cinemaDate,_mapOneByOne)
+    }
+
+    return {
+        getFunctions: getFunctions
+    };
+
+})();
         
